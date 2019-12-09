3d86aaa89b43355488319e50aea757127ea54686
Simon Chaveneau <simon.chaveneau@ariadnext.com>
2019-12-09 10:46:26 +0100

    Commit app and test dll

---
+++ TestApp/TestApp.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(TestApp.MainWindow.TestedMethod(4), Is.EqualTo(8));
+        }
+    }
+}
